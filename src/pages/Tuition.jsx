6777f8bc93489ac7d62f975bd8a6324946860423
arasruislam <arasru01@gmail.com>
2024-08-26 22:58:12 +0600

    fetch tuitions data

--- src/pages/Tuition.jsx
+++ src/pages/Tuition.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Tuition = () => {
+   const [tuitions, setTuitions] = useState([]);
+
+   useEffect(() => {
+      const fetchTuitions = async () => {
+         try {
+            const response = await axios.get(
+               "https://studysphere-dnn6.onrender.com/tuitions/list/"
+            );
+            setTuitions(response.data.results);
+         } catch (error) {
+            console.error("Error fetching data:", error);
+         }
+      };
+
+      fetchTuitions();
+   }, []);
+
    return (
       <>
          <div>hello</div>
