ae202d9701e96e4ea267bdc70bba52e15244abb5
arasruislam <arasru01@gmail.com>
2024-08-30 10:13:31 +0600

    ui done for application table.

--- src/Components/Sections/ApplicationTable.jsx
+++ src/Components/Sections/ApplicationTable.jsx
@@ -2,104 +2,31 @@ import React from 'react';
 
 const ApplicationTable = () => {
    return (
-      <div className="container mx-auto dark:text-gray-800">
+      <div className="mx-auto dark:text-gray-800">
          <h2 className="mb-4 text-2xl font-semibold leading-tight">
             All Applied Tuitions
          </h2>
          <div className="overflow-x-auto">
             <table className="min-w-full text-xs">
-               <colgroup>
+               {/* <colgroup>
                   <col />
                   <col />
                   <col />
                   <col />
                   <col />
                   <col className="w-24" />
-               </colgroup>
-               <thead className="dark:bg-gray-300">
+               </colgroup> */}
+               <thead className="bg-gray-300">
                   <tr className="text-left">
-                     <th className="p-3">Invoice #</th>
-                     <th className="p-3">Client</th>
-                     <th className="p-3">Issued</th>
-                     <th className="p-3">Due</th>
-                     <th className="p-3 text-right">Amount</th>
+                     <th className="p-3">No.</th>
+                     <th className="p-3">Title</th>
+                     <th className="p-3">Apply At</th>
+                     <th className="p-3">Level</th>
                      <th className="p-3">Status</th>
                   </tr>
                </thead>
                <tbody>
-                  <tr className="border-b border-opacity-20 dark:border-gray-300 dark:bg-gray-50">
-                     <td className="p-3">
-                        <p>97412378923</p>
-                     </td>
-                     <td className="p-3">
-                        <p>Microsoft Corporation</p>
-                     </td>
-                     <td className="p-3">
-                        <p>14 Jan 2022</p>
-                        <p className="dark:text-gray-600">Friday</p>
-                     </td>
-                     <td className="p-3">
-                        <p>01 Feb 2022</p>
-                        <p className="dark:text-gray-600">Tuesday</p>
-                     </td>
-                     <td className="p-3 text-right">
-                        <p>$15,792</p>
-                     </td>
-                     <td className="p-3 text-right">
-                        <span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">
-                           <span>Pending</span>
-                        </span>
-                     </td>
-                  </tr>
-                  <tr className="border-b border-opacity-20 dark:border-gray-300 dark:bg-gray-50">
-                     <td className="p-3">
-                        <p>97412378923</p>
-                     </td>
-                     <td className="p-3">
-                        <p>Tesla Inc.</p>
-                     </td>
-                     <td className="p-3">
-                        <p>14 Jan 2022</p>
-                        <p className="dark:text-gray-600">Friday</p>
-                     </td>
-                     <td className="p-3">
-                        <p>01 Feb 2022</p>
-                        <p className="dark:text-gray-600">Tuesday</p>
-                     </td>
-                     <td className="p-3 text-right">
-                        <p>$275</p>
-                     </td>
-                     <td className="p-3 text-right">
-                        <span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">
-                           <span>Pending</span>
-                        </span>
-                     </td>
-                  </tr>
-                  <tr className="border-b border-opacity-20 dark:border-gray-300 dark:bg-gray-50">
-                     <td className="p-3">
-                        <p>97412378923</p>
-                     </td>
-                     <td className="p-3">
-                        <p>Coca Cola co.</p>
-                     </td>
-                     <td className="p-3">
-                        <p>14 Jan 2022</p>
-                        <p className="dark:text-gray-600">Friday</p>
-                     </td>
-                     <td className="p-3">
-                        <p>01 Feb 2022</p>
-                        <p className="dark:text-gray-600">Tuesday</p>
-                     </td>
-                     <td className="p-3 text-right">
-                        <p>$8,950,500</p>
-                     </td>
-                     <td className="p-3 text-right">
-                        <span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">
-                           <span>Pending</span>
-                        </span>
-                     </td>
-                  </tr>
-                  <tr className="border-b border-opacity-20 dark:border-gray-300 dark:bg-gray-50">
+                  <tr className="border-b border-opacity-20 border-gray-300 bg-gray-50">
                      <td className="p-3">
                         <p>97412378923</p>
                      </td>
@@ -114,14 +41,11 @@ const ApplicationTable = () => {
                         <p>01 Feb 2022</p>
                         <p className="dark:text-gray-600">Tuesday</p>
                      </td>
-                     <td className="p-3 text-right">
-                        <p>$98,218</p>
-                     </td>
                      <td className="p-3 text-right flex gap-2">
-                        <span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">
+                        <span className="px-3 py-1 font-semibold rounded-md bg-[#FFC338] text-[#FFC338] bg-opacity-15">
                            <span>Pending</span>
                         </span>
-                        <span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">
+                        <span className="px-3 py-1 font-semibold rounded-md bg-[#3890d8] text-[#3890d8] bg-opacity-15">
                            <span>Approved</span>
                         </span>
                      </td>
